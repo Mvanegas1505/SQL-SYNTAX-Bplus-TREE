4e1d83164209113b50d5461a07a8218aecbd18b2
Mvanegas1505 <141659905+Mvanegas1505@users.noreply.github.com>
2024-11-11 00:19:30 -0500

    Add files via upload

---
+++ EDA II PARCIAL 3(3)/ArbolBPlus.h
@@ -0,0 +1,74 @@
+#ifndef ARBOL_BPLUS_H
+#define ARBOL_BPLUS_H
+
+#include <string>    // Biblioteca para manejar cadenas de texto (std::string)
+#include <vector>    // Biblioteca para manejar listas de elementos (std::vector)
+#include <map>       // Biblioteca para manejar pares clave-valor (std::map)
+#include <iostream>  // Biblioteca para operaciones de entrada y salida (std::cout)
+#include "BpNode.h"
+#include <algorithm>
+
+// Clase ArbolBPlus simula un árbol B+ con métodos básicos para las operaciones de SQL.
+class ArbolBPlus {
+public:
+
+    BpNode* root; // Puntero a la raíz del árbol B+
+    int maxKeys;  // Máximo número de llaves permitidas por nodo
+
+    ArbolBPlus(int maxK) : root(nullptr), maxKeys(maxK) {}
+
+    // Método para simular la inserción en el árbol B+
+    // Recibe el nombre de la tabla, una lista de columnas y una lista de valores
+    void insertar(const std::string& tabla, const std::vector<std::string> columnas, const std::vector<std::string> valores);
+
+    // Método para simular la selección de datos en el árbol B+
+    // Recibe el nombre de la tabla y una lista de columnas
+    void seleccionar(const std::string& tabla, const std::vector<std::string>& columnas);
+
+    // Método para simular la actualización de datos en el árbol B+
+    // Recibe el nombre de la tabla, un mapa de asignaciones (columna=valor), y una condición
+    void actualizar(const std::string& tabla, const std::map<std::string, std::string>& asignaciones, const std::string& condicion);
+
+    // Método para simular la eliminación de datos en el árbol B+
+    // Recibe el nombre de la tabla y una condición para eliminar filas que la cumplan
+    void eliminar(const std::string& tabla, const std::string& condicion);
+
+    void eliminarClave(BpNode* nodo, Dato* clave);
+
+    void rebalancearDespuesDeEliminar(BpNode* nodo);
+
+    void redistribuirIzq(BpNode* padre, size_t idx);
+
+    void redistribuirDer(BpNode* padre, size_t idx);
+
+    void fusionarNodos(BpNode* padre, size_t idx);
+
+    BpNode* encontrarPadre(BpNode* nodoActual, BpNode* nodoHijo);
+
+    void eliminarClavePorNombre(BpNode* nodo, Dato* clave, const std::string& nombre);
+
+     void mostrarArbol();
+
+    
+
+
+    private:
+
+     // Inserta una llave en un nodo que no está lleno
+    void insertNonFull(BpNode* node, Dato* llave);
+
+    // Divide un nodo cuando está lleno
+    BpNode* splitchild(BpNode* node, int indice);
+
+    // Función auxiliar para recorrer a partir de un nodo específico
+    void GoThroughNode(BpNode* node);
+
+    // Función auxiliar para mostrar el árbol desde un nodo específico
+    void printTreeAux(BpNode* node, std::string prefix, bool isLast);
+
+    int extraerClaveDesdeCondicion(const std::string& condicion);
+    
+    BpNode* buscarNodo(BpNode* nodo, Dato* clave);
+};
+
+#endif 
---
+++ EDA II PARCIAL 3(3)/BpNode.h
@@ -0,0 +1,23 @@
+#ifndef NODE_H
+#define NODE_H 
+
+#include <vector>
+#include "Dato.h"
+
+class BpNode{
+
+public:
+ 
+ bool isLeaf;
+ std::vector<Dato*> key;
+ std::vector<BpNode*> child_ptrs;
+  BpNode* next;
+
+
+ BpNode(bool isNodeleaf) : isLeaf(isNodeleaf), next(nullptr){}
+
+};
+
+
+
+#endif
